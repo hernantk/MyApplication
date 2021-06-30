04cea5da723daa974ab7e6727002736f718051e7
hernan <Hernantkup@gmail.com>
2021-06-30 19:15:22 -0300

    Básico +-

---
+++ app/src/main/java/br/hernan/myapplication/ui/newmemory/Activity_new_memory.kt
@@ -0,0 +1,12 @@
+package br.hernan.myapplication.ui.newmemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.hernan.myapplication.R
+
+class ActivityNewMemory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_memory)
+    }
+}
